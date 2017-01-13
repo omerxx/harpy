6a574abe8cc1bce6ee8826b4c4123793246106bb
Omer Hamerman <omerxx@gmail.com>
2017-01-13 17:37:40 +0200

    open file for read. encode to etf8

--- dataminer.py
+++ dataminer.py
@@ -25,7 +25,7 @@ class performance(object):
     def __store_into_file(title, result):
         #store data collected into file
         har_file = open(title + '.json', 'w')
-        har_file.write(str(result))
+        har_file.write(result.encode('utf-8'))
        	har_file.close()
 
     def __start_server(self):
@@ -83,10 +83,13 @@ class performance(object):
     def fetch_urls(self, urlsFileName):
         #read urls from file
         urls = []
-        with open(urlsFileName, 'w') as urlsfile:
+        with open(urlsFileName, 'r') as urlsfile:
             for line in urlsfile:
                 urls.append(line)
         
+		if len(urls) == 0:
+			print 'Urls file empty'
+			exit(0)
         return urls
                 
 
@@ -94,18 +97,17 @@ if __name__ == '__main__':
 
 	urlsFileName = 'urls.txt'
     # for headless execution
-    with Xvfb() as xvfb:
-
-    	# parser = argparse.ArgumentParser(description='Performance Testing using Browsermob-Proxy and Python')
-    	# parser.add_argument('-u','--url',help='URL to test',required=True)
-    	# parser.add_argument('-b','--browser',help='Select Chrome or Firefox',required=True)
-    	# parser.add_argument('-p','--path',help='Select path for output files',required=False)
-    	# args = vars(parser.parse_args())
-    	
-        path = os.getenv('BROWSERMOB_PROXY_PATH', '/browsermob-proxy-2.1.2/bin/browsermob-proxy')
-    	RUN = performance(path)
-        #Currently takes only first line of file
-        url = RUN.fetch_urls()[0]
-    	RUN.start_all()
-    	RUN.create_har(url)
-    	RUN.stop_all()
+	with Xvfb() as xvfb:
+		# parser = argparse.ArgumentParser(description='Performance Testing using Browsermob-Proxy and Python')
+		# parser.add_argument('-u','--url',help='URL to test',required=True)
+		# parser.add_argument('-b','--browser',help='Select Chrome or Firefox',required=True)
+		# parser.add_argument('-p','--path',help='Select path for output files',required=False)
+		# args = vars(parser.parse_args())
+		path = os.getenv('BROWSERMOB_PROXY_PATH', '/browsermob-proxy-2.1.2/bin/browsermob-proxy')
+		RUN = performance(path)
+		#Currently takes only first line of file
+		url = RUN.fetch_urls(urlsFileName)[0]
+		RUN.start_all()
+		RUN.create_har(url)
+		print "%s: Done "%(datetime.now())
+		RUN.stop_all()
